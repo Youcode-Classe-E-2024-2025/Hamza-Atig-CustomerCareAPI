82981775c7261ca2300b548f4a56d276503e372c
Atig-Hamza <hamzaatig@icloud.com>
2025-03-21 10:25:10 +0000

    create tickets mogrations

---
+++ database/migrations/2025_03_21_102149_tickets.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['open', 'pending', 'closed'])->default('open');
+            $table->date('tacked_at')->nullable();
+            $table->date('closed_at')->nullable();
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('agent_id')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
