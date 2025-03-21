bd50e654f7a232de14c12b12e2b8e9b33ac0b2ab
Atig-Hamza <hamzaatig@icloud.com>
2025-03-21 14:36:57 +0000

    add responses service

---
+++ app/Services/ResponceService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\response;
+
+class ResponceService
+{
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = auth()->user();
+    }
+
+    public function index()
+    {
+        if (!$this->user) {
+            return response()->json(['message' => 'Call to a member function tickets() on null'], 500);
+        }
+        $responces = response::where('user_id', $this->user->id)->get();
+        if ($responces->count() === 0) {
+            return response()->json(['message' => 'You don\'t have any responces yet, create one first'], 404);
+        }
+        return $responces;
+    }
+
+    public function create(array $data)
+    {
+        return response::create($data);
+    }
+
+    public function show(response $responce)
+    {
+        if (!$responce->id) {
+            return response()->json(['message' => 'Responce not found'], 404);
+        }
+        return $responce;
+    }
+
+    public function update($responce, array $data)
+    {
+        $responce->update($data);
+        return response()->json(['message' => 'Responce updated successfully'], 403);
+    }
+
+    public function delete($responce)
+    {
+        $responce->delete();
+        return response()->json(['message' => 'Responce deleted successfully'], 200);
+    }
+}
