dd7f111ca657b5afff220982cea755326c7768f3
Atig-Hamza <hamzaatig@icloud.com>
2025-03-21 14:38:11 +0000

    add resourses routes of responses to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResponseController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,4 @@ Route::post('login', [AuthController::class, 'login'])->name('login')->middlewar
 Route::post('signup', [AuthController::class, 'signup']);   
 
 Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
+Route::apiResource('responces', ResponseController::class)->middleware('auth:sanctum');
