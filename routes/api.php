35cc657f525d33f96f1fcf242c8aab2a54ace96d
Atig-Hamza <hamzaatig@icloud.com>
2025-03-21 12:25:37 +0000

    add ticket api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -7,3 +8,5 @@ use App\Http\Controllers\AuthController;
 
 Route::post('login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::post('signup', [AuthController::class, 'signup']);   
+
+Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
